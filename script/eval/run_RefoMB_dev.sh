7fe74dfe4795e722427b09f94d241437009a86cb
Haoye Zhang <42668396+Haoye17@users.noreply.github.com>
2024-11-03 14:30:38 +0800

    Delete script/eval/run_RefoMB_dev.sh

--- script/eval/run_RefoMB_dev.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python ./RefoMB/eval_RefoMB_dev.py \
-    --answer_modelA    ./eval/data/RefoMB_gpt4v_dev.jsonl \
-    --answer_modelB    /home/dangyunkai/openmme_0511_eval/baseline/Mini-Gemini-34B_openmme_val.jsonl
